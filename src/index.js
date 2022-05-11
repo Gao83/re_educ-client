34e496fdb253ccf8aa294fe0930c41ca403e70b9
Isaac Miralles <isaacmiralles@gmail.com>
2022-05-11 18:12:00 +0200

    register/login started

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "../pages/HomePage"
+import UserSignupPage from "../pages/UserSignupPage"
+import UserLoginPage from "../pages/UserLoginPage"
 
 const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<HomePage />} />
+            <Route path='/registro' element={<UserSignupPage />} />
+            <Route path='/iniciar-sesion' element={<UserLoginPage />} />
         </Routes>
     )
 }
