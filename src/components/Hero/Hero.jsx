567cca8b562510d71b6cdd4a023b23874dff618e
yima_po <jesusmartinponz@gmail.com>
2022-05-11 18:13:04 +0200

    home page

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Hero = () => {
+
+    return (
+        <div className="hero-home">
+            <h1>HERO</h1>
+        </div>
+    )
+
+}
+
+export default Hero
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom"
-import HomePage from "../pages/HomePage"
+import HomePage from "../pages/HomePage/HomePage"
 
 const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<HomePage />} />
+
         </Routes>
     )
 }
