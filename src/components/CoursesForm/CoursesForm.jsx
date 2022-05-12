a8e259863d68121a473f01256849552d07280dcd
Cristian Calderon <cristianfab90@gmail.com>
2022-05-12 11:56:55 +0200

    Registro del curso

--- src/components/CoursesForm/CoursesForm.jsx
+++ src/components/CoursesForm/CoursesForm.jsx
@@ -1,73 +1,114 @@
 import { useState } from "react"
 import { Button, Container, Form } from "react-bootstrap"
+import courseServices from "../../services/courses.service"
 
 const CoursesForm = () => {
+    const [isPaid, setIsPaid] = useState(false)
+
 
     const [courseData, setCourseData] = useState({
         title: '',
-        img: '',
+        courseImg: '',
         headline: '',
         description: '',
         requirements: '',
         content: '',
         duration: '',
-        isPaid: false,
         price: 0,
         category: ''
     })
-    const { title, img, headline, description, requirements, content, duration, isPaid, price, category } = courseData
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.currentTarget
+        setCourseData({
+            ...courseData,
+            [name]: value
+        })
+    }
+    const handleIsPaidInput = (e) => {
+        setIsPaid(e.target.checked)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        courseServices
+            .createOneCourse(courseData)
+            .then(response => {
+                console.log(response)
+
+            })
+            .catch(err => console.log(err))
+
+    }
+
+
+    const { title, courseImg, headline, description, requirements, content, duration, price } = courseData
     return (
         <Container>
-            <Form>
-
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="title">
                     <Form.Label>Titulo del Curso</Form.Label>
-                    <Form.Control type="text" value={title} />
-                </Form.Group >
-                {/* INCLUIR IMAGENES PARA EL CURSOO */}
-                <Form.Group className="mb-3" controlId="headline">
-                    <Form.Label>Encabezado</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group >
-                <Form.Group className="mb-3" controlId="requirements">
-                    <Form.Label>Requisitos</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group >
-                <Form.Group className="mb-3" controlId="content">
-                    <Form.Label>Contenido del curso</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group >
-                <Form.Group className="mb-3" controlId="duration">
-                    <Form.Label>Duración</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control type="text" value={title} onChange={handleInputChange} name='title' />
                 </Form.Group >
 
-                <Form.Group className="mb-3" controlId="isPaid">
-                    <Form.Check label="Marca si es de pago?" />
+
+                <Form.Group className="mb-3" controlId="title">
+                    <Form.Label>URL del Curso</Form.Label>
+                    <Form.Control type="text" value={courseImg} onChange={handleInputChange} name='courseImg' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="headline">
+                    <Form.Label>Encabezado</Form.Label>
+                    <Form.Control type="text" value={headline} onChange={handleInputChange} name='headline' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="description">
+                    <Form.Label>Descripción</Form.Label>
+                    <Form.Control type="text" value={description} onChange={handleInputChange} name='description' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="requirements">
+                    <Form.Label>Requisitos</Form.Label>
+                    <Form.Control type="text" value={requirements} onChange={handleInputChange} name='requirements' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="content">
+                    <Form.Label>Contenido del curso</Form.Label>
+                    <Form.Control type="text" value={content} onChange={handleInputChange} name='content' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="duration">
+                    <Form.Label>Duración</Form.Label>
+                    <Form.Control type="text" value={duration} onChange={handleInputChange} name='duration' />
+                </Form.Group >
+
+                <Form.Group className="mb-3" controlId="ispaid">
+                    <Form.Label>Marca si es de pago</Form.Label>
+                    <br />
+                    <Form.Check type="checkbox" checked={isPaid} onChange={handleIsPaidInput} name="isPaid" />
                 </Form.Group >
 
                 <Form.Group className="mb-3" controlId="price">
                     <Form.Label>Precio</Form.Label>
-                    <Form.Control type="number" />
+                    <Form.Control type="number" value={price} onChange={handleInputChange} name='price' />
                 </Form.Group >
+                <Form.Label>Elige Categoría</Form.Label>
+                <Form.Select aria-label="Default select example" onChange={handleInputChange} name='category' >
+                    <option value="tecnologia">Técnología</option>
+                    <option value="diseno">Diseño</option>
+                    <option value="marketing">Marketing</option>
+                    <option value="informatica y software">Informática y Software</option>
+                    <option value="musica">Música</option>
+                    <option value="salud y fitness">Salud y Fitness</option>
+                    <option value="otros">Otros</option>
+                </Form.Select>
 
-                <Form.Group className="mb-3" controlId="category" >
-                    <Form.Label>Elije Categoría</Form.Label>
-                    <br />
-                    <Form.Check name="category" label="Técnología" inline type="radio" />
-                    <Form.Check name="category" label="Diseño" inline type="radio" />
-                    <Form.Check name="category" label="Marketing" inline type="radio" />
-                    <Form.Check name="category" label="Informática y Software" inline type="radio" />
-                    <Form.Check name="category" label="Música" inline type="radio" />
-                    <Form.Check name="category" label="Salud y Fitness" inline type="radio" />
-                    <Form.Check name="category" label="Otros" inline type="radio" />
-                </Form.Group >
                 <Button variant="dark" type="submit">Crear nuevo curso</Button>
             </Form>
         </Container>
     )
 
 }
-// name = "firstName" value = { signupForm.firstName } onChange = { handleInputChange }
 
 export default CoursesForm
